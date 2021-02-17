1c4d576eae0d60df5cd09d05fe3bb932590bcdfa
mamohamed252 <ma.mohamed252@gmail.com>
2021-02-16 19:55:05 -0600

    Completed Part 2 SQL Exercise

--- Example Practice/HTML Ex/exercise.sql_simple-queries-master/MohamedMohamed.sql
+++ Example Practice/HTML Ex/exercise.sql_simple-queries-master/MohamedMohamed.sql
@@ -7,4 +7,14 @@ USE classicmodels;
  
  SELECT productName, productLine, buyPrice
  FROM products
- ORDER BY buyPrice DESC; 
+ ORDER BY buyPrice DESC;
+ 
+ /* Part 2
+ Write a query to display the first name, last name, and city for all customers from Germany.
+ Columns should display as `First Name`, `Last Name`, and `City`.
+The output should be sorted by the customer’s last name (ascending).*/
+
+ SELECT contactFirstName,  contactLastName, city
+ FROM customers
+ WHERE country = 'Germany'
+ ORDER BY  contactLastName ASC;
