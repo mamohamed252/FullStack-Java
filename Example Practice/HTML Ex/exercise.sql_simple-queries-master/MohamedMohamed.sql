49f19a9ab8d61fb3efc0ee969b50c2b8a9e1dbc1
mamohamed252 <ma.mohamed252@gmail.com>
2021-02-16 20:00:33 -0600

    Completed Part 3 SQL Exercise

--- Example Practice/HTML Ex/exercise.sql_simple-queries-master/MohamedMohamed.sql
+++ Example Practice/HTML Ex/exercise.sql_simple-queries-master/MohamedMohamed.sql
@@ -18,3 +18,13 @@ The output should be sorted by the customer’s last name (ascending).*/
  FROM customers
  WHERE country = 'Germany'
  ORDER BY  contactLastName ASC;
+ 
+  /* Part 3
+ Write a query to display each of the unique values of the status field in the orders table.
+ The output should be sorted alphabetically increasing.
+ Hint: the output should show exactly 6 rows.*/
+ 
+SELECT DISTINCT STATUS
+FROM orders
+ORDER BY STATUS ASC;
+ 
